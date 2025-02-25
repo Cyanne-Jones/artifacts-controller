c12cd800e9de2d17120a3f963f0a50595ee0b8b4
Cyanne Jones <jonescyanne@yahoo.com>
2025-02-24 19:41:34 -0700

    add basic unequip function

---
+++ action/unequip.js
@@ -0,0 +1,43 @@
+require('dotenv').config()
+const { TOKEN, API_BASE_URL, CHARACTER } = process.env
+
+// run this with `node action/unequip.js slot=helmet character=Flyanne`
+
+// slot = helmet | weapon | shield etc.
+
+async function basic() {
+
+  args = process.argv
+
+  const slot = args.find(arg => arg.startsWith("slot=")).split('=')[1] || "weapon"
+  const parsedCharacter = args.find(arg => arg.startsWith("character="))?.split('=')[1] || CHARACTER 
+
+  const body = `{ "slot": "${slot}" }`
+
+  console.log(`Unequipping from: ${slot} slot...`)
+
+  const url = `${API_BASE_URL}/my/${parsedCharacter}/action/unequip`
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+      Authorization: 'Bearer ' + TOKEN
+    },
+    body
+  }
+
+  try {
+    const response = await fetch(url, options);
+    if (!response.ok) {
+      console.log({ response })
+      throw new Error(`Failed to unequip item from ${slot} slot`)
+    }
+    const { data } = await response.json();
+    console.log('Unequip successful:', { data })
+  } catch (error) {
+    console.log({ error })
+  }
+}
+
+basic()
